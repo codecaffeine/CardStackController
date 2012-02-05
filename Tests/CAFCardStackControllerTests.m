eb8ea3a504e8bdb522074834c77ab959342a2e41
Matt Thomas <matt@codecaffeine.com>
2012-02-05 14:06:09 -0500

    Adding Tests
    * testCardStackControllerInitializesEmptyArray
    * testAddCardViewControllerAddsToArray

--- Tests/CAFCardStackControllerTests.m
+++ Tests/CAFCardStackControllerTests.m
@@ -8,12 +8,28 @@
 
 #import "CAFCardStackControllerTests.h"
 #import "CAFCardStackController.h"
+#import "CAFRandomColorViewController.h"
 
 @implementation CAFCardStackControllerTests
 
-- (void)testAddCardViewControllerAddsToCardViewControllerArray
+- (void)testCardStackControllerInitializesEmptyArray
 {
 	CAFCardStackController *cardStackController = [[CAFCardStackController alloc] init];
+	STAssertNotNil(cardStackController.cardViewControllers, 
+				   @"%@: cardViewControllers property shouldn't be nil, but it is", 
+				   NSStringFromClass([self class]));
+
+}
+
+- (void)testAddCardViewControllerAddsToArray
+{
+	CAFCardStackController *cardStackController = [[CAFCardStackController alloc] init];
+	CAFRandomColorViewController *randomColorViewController = [[CAFRandomColorViewController alloc] init];
+	[cardStackController addCardViewController:randomColorViewController];
+	STAssertTrue([cardStackController.cardViewControllers count] == 1, 
+				 @"%@: should have one object, but has %d", 
+				 NSStringFromClass([self class]), 
+				 [cardStackController.cardViewControllers count]);
 }
 
 @end
