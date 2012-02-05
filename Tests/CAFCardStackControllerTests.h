80e3ffeba20aaf683857afb9236f27b7bd4eb118
Matt Thomas <matt@codecaffeine.com>
2012-02-05 13:49:24 -0500

    Adding CardStackControllerTests class

---
+++ Tests/CAFCardStackControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  CAFCardStackControllerTests.h
+//  Card Stack Controller Demo
+//
+//  Created by Matthew Thomas on 2/5/12.
+//  Copyright (c) 2012 Code/Caffeine. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CAFCardStackControllerTests : SenTestCase
+
+@end
---
+++ Tests/CAFCardStackControllerTests.m
@@ -0,0 +1,19 @@
+//
+//  CAFCardStackControllerTests.m
+//  Card Stack Controller Demo
+//
+//  Created by Matthew Thomas on 2/5/12.
+//  Copyright (c) 2012 Code/Caffeine. All rights reserved.
+//
+
+#import "CAFCardStackControllerTests.h"
+#import "CAFCardStackController.h"
+
+@implementation CAFCardStackControllerTests
+
+- (void)testAddCardViewControllerAddsToCardViewControllerArray
+{
+	CAFCardStackController *cardStackController = [[CAFCardStackController alloc] init];
+}
+
+@end
