8313a199478c1eda30391be148eef76903cbc666
Matt Thomas <matt@codecaffeine.com>
2012-02-05 13:54:23 -0500

    Adding RandomColorViewController class

---
+++ App/CAFRandomColorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAFRandomColorViewController.h
+//  Code/Caffeine Utilities
+//
+//  Created by Matthew Thomas on 12/22/11.
+//  Copyright (c) 2011 Code/Caffeine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAFRandomColorViewController : UIViewController
+@end
+
