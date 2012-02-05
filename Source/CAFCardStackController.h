d1b1491d166634b263d17bde7f779035f63ffae0
Matt Thomas <matt@codecaffeine.com>
2012-02-05 11:25:32 -0500

    Adding CAFCardStackController
    Methods and properties are stubbed out but not 
    yet implemented

---
+++ Source/CAFCardStackController.h
@@ -0,0 +1,17 @@
+//
+//  CAFCardStackController.h
+//  Card Stack Controller Demo
+//
+//  Created by Matthew Thomas on 2/5/12.
+//  Copyright (c) 2012 Code/Caffeine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAFCardStackController : UIViewController
+@property (readonly) NSArray *cardViewControllers;
+- (void)addCardViewController:(UIViewController *)viewController;
+- (void)focusCardViewController:(UIViewController *)viewController;
+- (void)showAllCardViewControllers;
+- (void)removeCardViewController:(UIViewController *)viewController;
+@end
---
+++ Source/CAFCardStackController.m
@@ -0,0 +1,79 @@
+//
+//  CAFCardStackController.m
+//  Card Stack Controller Demo
+//
+//  Created by Matthew Thomas on 2/5/12.
+//  Copyright (c) 2012 Code/Caffeine. All rights reserved.
+//
+
+#import "CAFCardStackController.h"
+
+@interface CAFCardStackController ()
+
+@end
+
+@implementation CAFCardStackController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+
+#pragma mark - Properties
+- (NSArray *)cardViewControllers
+{
+	return nil;
+}
+
+
+#pragma mark - Instance Methods
+- (void)addCardViewController:(UIViewController *)viewController
+{
+	
+}
+
+
+- (void)focusCardViewController:(UIViewController *)viewController
+{
+	
+}
+
+
+- (void)showAllCardViewControllers
+{
+	
+}
+
+
+- (void)removeCardViewController:(UIViewController *)viewController
+{
+	
+}
+
+@end
