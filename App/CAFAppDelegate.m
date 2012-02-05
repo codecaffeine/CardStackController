d5a6ce8a385fc2e14274b2e8ec5dc30d1604e1dc
Matt Thomas <matt@codecaffeine.com>
2012-02-05 14:18:44 -0500

    Adding CAFCardStackController
    self.window.rootViewController = 
    CAFCardStackController.

--- App/CAFAppDelegate.m
+++ App/CAFAppDelegate.m
@@ -7,15 +7,18 @@
 //
 
 #import "CAFAppDelegate.h"
+#import "CAFCardStackController.h"
 
 @implementation CAFAppDelegate
-
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+	
+	CAFCardStackController *cardStackController = [[CAFCardStackController alloc] init];
+	self.window.rootViewController= cardStackController;
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
