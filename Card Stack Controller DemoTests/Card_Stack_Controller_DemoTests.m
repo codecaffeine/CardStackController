45c44cbf19a8075b68c5f59d310fa508724f74e2
Matt Thomas <matt@codecaffeine.com>
2012-02-05 09:56:13 -0500

    Initial Commit

---
+++ Card Stack Controller DemoTests/Card_Stack_Controller_DemoTests.m
@@ -0,0 +1,32 @@
+//
+//  Card_Stack_Controller_DemoTests.m
+//  Card Stack Controller DemoTests
+//
+//  Created by Matthew Thomas on 2/5/12.
+//  Copyright (c) 2012 Code/Caffeine. All rights reserved.
+//
+
+#import "Card_Stack_Controller_DemoTests.h"
+
+@implementation Card_Stack_Controller_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Card Stack Controller DemoTests");
+}
+
+@end
